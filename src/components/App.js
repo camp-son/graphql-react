fc3437e2b062cf13375ea7bf68f067c522751f6b
camp-son <sjy2814@gmail.com>
2021-12-31 16:52:31 +0900

    feat: Initialize router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Detail from '../routes/Detail';
+import Home from '../routes/Home';
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/:id" element={<Detail />} />
+    </Routes>
   );
 }
 
