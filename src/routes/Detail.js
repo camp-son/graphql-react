5cff743dda98858fbdfe0ad5e5b1707899124347
camp-son <sjy2814@gmail.com>
2022-01-01 16:46:59 +0900

    feat: fetching query & styled component

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,76 @@
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const GET_MOVIE = gql`
+query getMovie($id: Int!) {
+  movie(id: $id) {
+    title
+    medium_cover_image
+    language
+    rating
+    description_intro
+  }
+}
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+const SubTitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+const Description = styled.p`
+  font-size: 28px;
+`;
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${props => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
+
 const Detail = () => {
-  return 'Detail';
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: {
+      id: Number(id)
+    }
+  });
+
+  if (loading) {
+    return 'loading...';
+  }
+
+  return (
+    <Container>
+      <Column>
+        <Title>{ data.movie.title }</Title>
+        <SubTitle>{ data.movie.language } { data.movie.rating } </SubTitle>
+        <Description>{ data.movie.description_intro }</Description>
+      </Column>
+      <Poster bg={ data.movie.medium_cover_image }></Poster>
+    </Container>
+  )
 }
 
 export default Detail;
